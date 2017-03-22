f009255de7e3042df27857858b90c8fe0979565e
Kyle Erwin <kyle.erwin24@gmail.com>
2017-03-22 12:18:51 +0200

    Created Skeleton

--- Notifications.java
+++ Notifications.java
@@ -1,26 +1,26 @@
 public class Notifications
 {
-	public void (String notification)
+	public void PushNotificationToAll (String notification)
 	{
 	}
 	
-	public void (String notification)
+	public void PushTargetedNotification (String notification)
 	{
 	}
 
-	public void (String notification)
+	public void SendEmailToAll(String html)
 	{
 	}
 
-	public void (String notification)
+	public void SendTargetedEmail(String notification)
 	{
 	}
 
-	public void (String notification)
+	public void SendSmsToAll (String notification)
 	{
 	}
 
-	public void (String notification)
+	public void SendTargetedSms (String notification)
 	{
 	}		
 }
