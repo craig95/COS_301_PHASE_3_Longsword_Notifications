5de1b205ce8503e3c787b24fe6bd2971b0dadf7b
Kyle Erwin <kyle.erwin24@gmail.com>
2017-03-22 12:31:05 +0200

    function descriptions

--- Notifications.java
+++ Notifications.java
@@ -6,27 +6,47 @@ public class Notifications
     {
 
     }
-
+    /**
+     * @param String notification - The notification we wish to push 
+     */
 	public void PushNotificationToAll (String notification)
 	{
 	}
 
+	/**
+     * @param String notification - The notification we wish to push 
+     * @param List<User> users - The users we want to send the notification to 
+     */
 	public void PushTargetedNotification (List<User> users, String notification)
 	{
 	}
 
+	/**
+     * @param String notification - The notification we wish to send via email
+     */
 	public void SendEmailToAll(String html)
 	{
 	}
 
+		/**
+     * @param String notification - The notification we wish to send via email
+     * @param List<User> users - The users we want to send the notification to 
+     */
 	public void SendTargetedEmail(List<User> users, String notification)
 	{
 	}
 
+	/**
+     * @param String notification - The notification we wish to send via sms
+     */
 	public void SendSmsToAll (String notification)
 	{
 	}
 
+	/**
+     * @param String notification - The notification we wish to send via sms
+     * @param List<User> users - The users we want to send the notification to 
+     */
 	public void SendTargetedSms (List<User> users, String notification)
 	{
 	}
