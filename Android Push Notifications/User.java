b40c8aa7e5e4d3f480035238d050e480febbe7c7
Kyle Erwin <kyle.erwin24@gmail.com>
2017-04-19 12:06:16 +0200

    Dummy user class

---
+++ Android Push Notifications/User.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Kyle on 4/19/2017.
+ */
+public class User
+{
+    private int ID = 0;
+
+    User()
+    {
+
+    }
+
+    User (int ID)
+    {
+        this.ID = ID;
+    }
+
+    public int getID()
+    {
+        return ID;
+    }
+
+    public void setID(int ID)
+    {
+        this.ID = ID;
+    }
+}
